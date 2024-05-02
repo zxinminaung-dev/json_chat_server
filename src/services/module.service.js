5fe54fce7b0097ae05e692dfabf6d8a9bd7d8399
zxinminaung-dev <146321635+zxinminaung-dev@users.noreply.github.com>
2024-05-03 03:01:29 +0630

    commit

--- src/services/module.service.js
+++ src/services/module.service.js
@@ -10,8 +10,19 @@ GetModuleList = async (id) =>{
             replacements:[id],
             type: Sequelize.QueryTypes.SELECT
         })
-        result.forEach(module => {
-            
+        result.forEach(async (module) => {
+            var taskQurey ="select * from Tasks where ModuleId=?"
+            var data = {
+                Id : 0,
+                Name : '',
+                Tasks:[]
+            }
+            data.Id = module.Id
+            data.Name = module.Name
+            data.Tasks = await dbContext.sequelize.query(taskQurey,{
+                replacements:[id],
+                type: Sequelize.QueryTypes.SELECT
+            })
         });
     }catch(err){
         response.success = false;
